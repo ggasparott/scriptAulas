a73b4b67a71f2aae2cf6d69fa4561e0e90ed2fb7
Gabriel Gasparotto Pantaleão <gabrielgasparotto45@gmail.com>
2025-03-13 15:25:59 +0000

    Exerc Aula

--- teste.js
+++ teste.js
@@ -1,5 +1,41 @@
-for(let i = 0; i < 10; i++){
-    for(let j = 0; j < 10; j++){
-        console.log(j, i);
-    }
+
+/*const numeros = [];
+for (let i = 0; i < 25; i++) {
+
+  let num = Math.floor(Math.random() * 10 ) + 1;
+
+
+  numeros.push(num);
 }
+
+
+const maiorNumero = Math.max(...numeros);
+const menorNumero = Math.min(...numeros);
+
+
+console.log('Maior número:', maiorNumero);
+console.log('Menor número:', menorNumero);
+
+const prompt = require('prompt-sync')();
+let num2 = prompt("Entre com um numero: ");
+num2 = Number(num2);
+if (num2 % 2 != 0) {
+  console.log("Impar");
+}
+*/
+
+
+
+
+let consumoPorRefeicao = parseFloat(prompt("Digite a quantidade de ração consumida pelo cachorro por refeição (em gramas): "));
+let pacoteRacaoKg = parseFloat(prompt("Digite o peso do pacote de ração (em kg): "));
+
+
+let pacoteRacaoGramas = pacoteRacaoKg * 1000;
+
+
+let numRefeicoes = Math.floor(pacoteRacaoGramas / consumoPorRefeicao);
+
+console.log(`Com o pacote de ${pacoteRacaoKg} kg de ração, é possível servir ${numRefeicoes} refeições.`);
+
+
